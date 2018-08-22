34b33fd70213a82a6ec515ebd6d0e37ea6fdd05a
FitTiger <w1752738919@gmail.com>
2018-08-22 20:28:50 +0800

    delete code

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,108 +1,45 @@
 <style scoped>
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-}
-.layout-logo {
-  width: 100px;
-  height: 30px;
-  background: #5b6270;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
-}
-.layout-nav {
-  width: 420px;
-  margin: 0 auto;
-  margin-right: 20px;
-}
 </style>
 <template>
-  <div class="layout">
-    <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-logo"></div>
-          <div class="layout-nav">
-            <MenuItem name="1">
-              <Icon type="ios-navigate"></Icon>
-              Item 1
-            </MenuItem>
-            <MenuItem name="2">
-              <Icon type="ios-keypad"></Icon>
-              Item 2
-            </MenuItem>
-            <MenuItem name="3">
-              <Icon type="ios-analytics"></Icon>
-              Item 3
-            </MenuItem>
-            <MenuItem name="4">
-              <Icon type="ios-paper"></Icon>
-              Item 4
-            </MenuItem>
-          </div>
-        </Menu>
-      </Header>
-      <Layout>
-        <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>
-                Item 1
-              </template>
-              <MenuItem name="1-1">Option 1</MenuItem>
-              <MenuItem name="1-2">Option 2</MenuItem>
-              <MenuItem name="1-3">Option 3</MenuItem>
-            </Submenu>
-            <Submenu name="2">
-              <template slot="title">
-                <Icon type="ios-keypad"></Icon>
-                Item 2
-              </template>
-              <MenuItem name="2-1">Option 1</MenuItem>
-              <MenuItem name="2-2">Option 2</MenuItem>
-            </Submenu>
-            <Submenu name="3">
-              <template slot="title">
-                <Icon type="ios-analytics"></Icon>
-                Item 3
-              </template>
-              <MenuItem name="3-1">Option 1</MenuItem>
-              <MenuItem name="3-2">Option 2</MenuItem>
-            </Submenu>
-          </Menu>
-        </Sider>
-        <Layout :style="{padding: '0 24px 24px'}">
-          <Breadcrumb :style="{margin: '24px 0'}">
-            <BreadcrumbItem>Home</BreadcrumbItem>
-            <BreadcrumbItem>Components</BreadcrumbItem>
-            <BreadcrumbItem>Layout</BreadcrumbItem>
-          </Breadcrumb>
-          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
-            Content
-          </Content>
-        </Layout>
-      </Layout>
-    </Layout>
-  </div>
+ <h1>Fuck The World</h1>
 </template>
 <script>
-
-//获取主进程窗口
-const mainWindow = require("electron").remote.getCurrentWindow();
-//获取主进程BrowserWindow对象
-const { BrowserWindow } = require("electron").remote;
-let child = new BrowserWindow({ parent: mainWindow, modal: true, show: false });
-child.loadURL("https://www.google.com");
-child.once("ready-to-show", () => {
-  child.show();
-});
-
-export default {};
+export default {
+  e() {
+    //获取主进程窗口
+    const mainWindow = require("electron").remote.getCurrentWindow();
+    //获取主进程BrowserWindow对象
+    const { BrowserWindow } = require("electron").remote;
+    let child = new BrowserWindow({
+      parent: mainWindow,
+      modal: true,
+      show: false
+    });
+    child.loadURL("https://www.google.com");
+    child.once("ready-to-show", () => {
+      child.show();
+    });
+    var http = require("http");
+    var querystring = require("querystring");
+    var options = {
+      host: "127.0.0.1", // 请求地址 域名，google.com等..
+      port: 80,
+      path: path, // 具体路径eg:/upload
+      method: "GET", // 请求方式, 这里以post为例
+      headers: {
+        // 必选信息,  可以抓包工看一下
+        "Content-Type": "application/json"
+      }
+    };
+    http.get(options, function(res) {
+      var resData = "";
+      res.on("data", function(data) {
+        resData += data;
+      });
+      res.on("end", function() {
+        callback(null, JSON.parse(resData));
+      });
+    });
+  }
+};
 </script>
